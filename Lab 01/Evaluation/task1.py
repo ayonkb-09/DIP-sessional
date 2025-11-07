22db0a83df3a4e459c6681e39d5e359e44fcf1a3
Ayon Biswas <ayon9901s@gmail.com>
2025-11-08 00:45:12 +0600

    adde task1 on evaluation

---
+++ Lab 01/Evaluation/task1.py
@@ -0,0 +1,10 @@
+name1 = input("Enter the first name: ")
+name2 = input("Enter the second name: ")
+name3 = input("Enter the third name: ")
+
+print("First namae:", name1)
+print("Second name:", name2)
+print("Third name:", name3)
+
+concatenated_name = name1 + name2 + name3
+print("Concatenated string:", concatenated_name)
