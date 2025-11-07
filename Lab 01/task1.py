9c137322138a99fbd189174bf2bbe11011bdc583
Ayon Biswas <ayon9901s@gmail.com>
2025-11-07 21:32:25 +0600

    add task1

---
+++ Lab 01/task1.py
@@ -0,0 +1,5 @@
+value1=int(input("Enter the first number:"))
+value2=int(input("Enter the second number:"))
+value3=int(input("Enter the third number:"))
+total_sum=value1+value2+value3
+print(total_sum)
