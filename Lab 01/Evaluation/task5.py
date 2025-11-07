260ff5d032d5dee7f29536e0eeacd4b3c4eb5dc9
Ayon Biswas <ayon9901s@gmail.com>
2025-11-08 00:49:55 +0600

    add task5 on evaluation

---
+++ Lab 01/Evaluation/task5.py
@@ -0,0 +1,9 @@
+# Assuming 'concatenated_string' is available from a previous step
+# If not, you would need to get the name inputs again and concatenate them here.
+# For demonstration, I'll use a sample concatenated string:
+concatenated_string = "Ayon Kumar Biswas"
+
+print("Full Name:", concatenated_string)
+print("First 5 characters:", concatenated_string[:5])
+print("From character 5 to the end:", concatenated_string[5:])
+print("Last 3 characters using negative indexing:", concatenated_string[-3:])
