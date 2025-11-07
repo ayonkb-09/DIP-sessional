c8ea2a1e2d180e834759981c1705e004c906a952
Ayon Biswas <ayon9901s@gmail.com>
2025-11-08 00:36:09 +0600

    add task6

---
+++ Lab 01/task6.py
@@ -0,0 +1,14 @@
+sample_string = "Hello, World!"
+slice_from_start = sample_string[:5]
+print("Slice from the start:", slice_from_start)
+slice_to_end = sample_string[7:]
+print("Slice to the end:", slice_to_end)
+slice_negative_index = sample_string[-6:-1]
+print("Slice using negative indexing:", slice_negative_index)
+print("\nComplete String Slicing Demonstration:")
+print("Original String:", sample_string)
+print("Slice [0:5]:", sample_string[0:5])
+print("Slice [7:]:", sample_string[7:])
+print("Slice [-6:-1]:", sample_string[-6:-1])
+
+
