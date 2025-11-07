0c2d4a21dd52699cf175f39403b2abd9b76e73cd
Ayon Biswas <ayon9901s@gmail.com>
2025-11-08 00:33:26 +0600

    add task4

---
+++ Lab 01/task4.py
@@ -0,0 +1,9 @@
+def print_fibonacci_series(n):
+  a, b = 0, 1
+  print("Fibonacci Series:")
+  for _ in range (n):
+    print (a, end=" ")
+    a, b = b, a + b
+  print()
+num_terms = 10
+print_fibonacci_series (num_terms)
