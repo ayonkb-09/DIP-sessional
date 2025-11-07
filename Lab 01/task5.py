a6bc58d7d287f393468dd6b57f2933de08fdcae1
Ayon Biswas <ayon9901s@gmail.com>
2025-11-08 00:35:09 +0600

    add task5

---
+++ Lab 01/task5.py
@@ -0,0 +1,11 @@
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+string3 = input("Enter the third string: ")
+
+print("First string:", string1)
+print("Second string:", string2)
+print("Third string:", string3)
+
+concatenated_string = string1 + string2 + string3
+print("Concatenated string:", concatenated_string)
