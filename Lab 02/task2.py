1941b2302f2f24c5c4ef066ef471cc07a539ec20
Ayon Biswas <ayon9901s@gmail.com>
2025-12-23 09:42:57 +0600

    task2 changed

--- Lab 02/task2.py
+++ Lab 02/task2.py
@@ -2,26 +2,31 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.imread(r"D:\3rd Year 2nd Semester\Mine\CSE-3216 Digital Image Processing Sessional\DIP Sessional\Lab 02\cat.jpeg")
 
-img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #convert the input image to grayscale
-height=img_gray.shape[0] #shape [0] for no of rows
-width=img_gray.shape[1] #shape[1] for no of columns
+img = cv2.imread(r"D:\3rd Year 2nd Semester\CSE-3216 Digital Image Processing Sessional\DIP Sessional\Lab 02\cat.jpeg")
 
-#input a zero matrix of the same size of the image
-img_mirrored=np.zeros((height,width))
+# IMPORTANT CHECK
+if img is None:
+    raise FileNotFoundError(f"Could not read image at: {img_path}")
 
-for i in range(height): #flip img gray on y-axis
- for j in range (width):
-   img_mirrored[i] [j]=img_gray[i] [width-j-1]
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#show the image in subplot
-plt.subplot (1,2,1)
-plt.imshow (img_gray,cmap='gray')
+height, width = img_gray.shape
+
+img_mirrored = np.zeros((height, width), dtype=np.uint8)
+
+for i in range(height):
+    for j in range(width):
+        img_mirrored[i][j] = img_gray[i][width - j - 1]
+
+plt.subplot(1, 2, 1)
+plt.imshow(img_gray, cmap='gray')
 plt.title('Original Image')
+plt.axis('off')
 
-plt.subplot (1,2,2)
-plt.imshow (img_mirrored, cmap='gray')
+plt.subplot(1, 2, 2)
+plt.imshow(img_mirrored, cmap='gray')
 plt.title('Mirrored Image')
+plt.axis('off')
 
 plt.show()
